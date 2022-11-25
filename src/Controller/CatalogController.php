c3696382ebf9f6eba881d8ee067fbd0f3d3e7b02
MiKL5 <miklgaillard@gmail.com>
2022-11-25 11:29:41 +0100

    wip

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -5,33 +5,38 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Category;
+
 
 class CatalogController extends AbstractController
 {
     #[Route('/category', name: 'app_category')]
     public function category(): Response
     {
-        $categories = $repo->findAll();
+        $category = $repoCatalogue->findAll();
 
         return $this->render('catalog/category.html.twig', [
-            'controller_name' => 'category',
-            'categories' => $categories
+            'category' => $category
         ]);
     }
 
     #[Route('/subcategory', name: 'app_subcategory')]
     public function subcategory(): Response
     {
+        $subcategory = $repoCatalogue->findAll();
+
         return $this->render('catalog/subcategory.html.twig', [
-            'controller_name' => 'subcategory',
+            'subcategory' => 'subcategory',
         ]);
     }
 
     #[Route('/product', name: 'app_product')]
     public function product(): Response
     {
+        $product = $repoCatalogue->findAll();
+
         return $this->render('catalog/product.html.twig', [
-            'controller_name' => 'product',
+            'product' => $product
         ]);
     }
 }
