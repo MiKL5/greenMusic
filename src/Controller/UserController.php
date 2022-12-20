82efd6ad8f2e7d2991d3e3542816d9a18c7a3c71
MiKL5 <miklgaillard@gmail.com>
2022-12-20 12:25:33 +0100

    Setting up translation

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -16,3 +16,22 @@ class UserController extends AbstractController
         ]);
     }
 }
+
+// dénition de la route pour l'avatar de l'utilisateur
+// avatar:
+//     path: /avatar/{id}
+//     controller: App\Controller\AvatarController::show
+
+// class AvatarController extends AbstractController
+// {
+//     public function show(int $id)
+//     {
+//         // Récupérez l'avatar de l'utilisateur à partir de la base de données ou d'un autre emplacement de stockage
+//         $avatar = ...
+
+//         // Renvoyez l'avatar au client
+//         return new Response($avatar, 200, [
+//             'Content-Type' => 'image/jpeg'
+//         ]);
+//     }
+// }
